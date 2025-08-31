fe8c9f192ba5f13a6237a9cc12bfcc524063834b
Arvin Subramanian <158824994+ArvinAIEngineer@users.noreply.github.com>
2025-08-31 11:57:39 +0400

    Update mariaKnowledge.ts

--- src/components/chat/mariaKnowledge.ts
+++ src/components/chat/mariaKnowledge.ts
@@ -23,46 +23,49 @@ const eventsInfo = `
 
 const birthdaysAndAnniversariesInfo = `
 **January:**
-- Birthdays: Tony Dewan (1st), Varun Jalan (29th), Kavita Coulagi (27th), Taruna Abichandani (Spouse, 31st).
-- Anniversaries: Oscar & Elaine De Lima Pereira (13th), Ridhima & Angad Singh (27th).
+- Birthdays: Tony Dewan (1st), Varun Jalan (29th), Kavita Coulagi (27th), Taruna (Haresh Abichandani's spouse, 31st), Dayani (Sourabh Chattopadhyay's child, 3rd), Samrat (Nath Laud's child, 11th), Gauri (Sandeep Agarwal's child, 11th), Dhruv (Priya Coulagi Bakshi's child, 14th), Aryaveer (Anirudh Agrawal's child, 20th), Avyaan (Venkat Muppana's child, 10th).
+- Anniversaries: Oscar & Elaine De Lima Pereira (13th), Ridhima & Angad Singh (27th), Vartan & Vivin Mathias (28th), Gopal & Padmaraje Patwardhan (28th).
 
 **February:**
-- Birthdays: Priya Coulagi Bakshi (16th).
-- Anniversaries: Amit & Rasika Palekar (25th), Haresh & Taruna Abichandani (27th), Dhruv & Stuti Rajani (15th), Vikram & Shweta Puri (16th).
+- Birthdays: Priya Coulagi Bakshi (16th), Parthiv (Tony Dewan's child, 17th), Annika (Kavita Coulagi's child, 20th).
+- Anniversaries: Gaurang & Shibani Suctancar (1st), Amit & Rasika Palekar (25th), Haresh & Taruna Abichandani (27th), Dhruv & Stuti Rajani (15th), Vikram & Shweta Puri (16th).
 
 **March:**
-- Birthdays: Varun Chawla (3rd), Venkat Muppana (17th), Anirudh Agrawal (15th), Nath Laud (25th), Gopal Patwardhan (31st), Samvedita Chawla (Spouse, 25th).
+- Birthdays: Varun Chawla (3rd), Venkat Muppana (17th), Anirudh Agrawal (15th), Nath Laud (25th), Gopal Patwardhan (31st), Haresh Abichandani (17th), Samvedita (Varun Chawla's spouse, 25th), Azlaan (Ali Haji's child, 20th).
 
 **April:**
-- Birthdays: Asheen Laljee (11th), Attreya Sawantt (24th), Sandeep Agarwal (27th), Vikram Devraj (Spouse, 19th), Srimoyi Bhattacharya (Spouse, 24th).
+- Birthdays: Asheen Laljee (11th), Attreya Sawantt (24th), Sandeep Agarwal (27th), Mayura (Mangish Kakodkar's spouse, 11th), Vikram (Kavita Coulagi's spouse, 19th), Srimoyi (Sourabh Chattopadhyay's spouse, 24th), Lakshaya (Gaurav Sapra's child, 11th), Sara (Gaurang Suctancar's child, 10th), Liam (Nitin Bhatia's child, 10th), Atharv (Dhruv Rajani's child, 6th).
+- Anniversaries: Varun & Samvedita Chawla (3rd), Gaurav & Divya Sapra (26th).
 
 **May:**
-- Birthdays: Amit Palekar (10th), Richa Haji (Spouse, 13th), Ridhima Singh (31st), Mangish Kakodkar (2nd), Nitin Bhatia (23rd), Angad Singh (Spouse, 4th).
-- Anniversaries: Attreya & Tanvi Sawantt (21st), Nath & Kalyani Laud (11th), Sandeep & Shruti Agarwal (8th).
+- Birthdays: Amit Palekar (10th), Ridhima Singh (31st), Mangish Kakodkar (2nd), Nitin Bhatia (23rd), Richa (Ali Haji's spouse, 13th), Prerna (Rahul Khetrapal's spouse, 13th), Stuti (Dhruv Rajani's spouse, 6th), Aaryan (Venkat Muppana's child, 30th), Roohaan (Ridhima Singh's child, 30th), Keya (Mangish Kakodkar's child, 24th), Siya (Varun Chawla's child, 27th), Devashree (Attreya Sawantt's child, 22nd), Tarini (Priya Coulagi Bakshi's child, 12th).
+- Anniversaries: Attreya & Tanvi Sawantt (21st), Nath & Kalyani Laud (11th), Sandeep & Shruti Agarwal (8th), Rahul & Priya Agarwal (18th).
 
 **June:**
-- Birthdays: Gaurang Suctancar (4th), Rahul Khetrapal (14th), Tanvi Sawantt (Spouse, 17th).
+- Birthdays: Gaurang Suctancar (4th), Rahul Khetrapal (14th), Tanvi (Attreya Sawantt's spouse, 17th), Vivin (Vartan Mathias's spouse, 12th), Kalyani (Nath Laud's spouse, 2nd), Geet (Aakash Khaunte's child, 8th), Alexa (Oscar De Lima Pereira's child, 8th), Viara (Vartan Mathias's child, 11th), Shivani (Sandeep Verenkar's child, 21st), Seher (Varun Chawla's child, 4th).
 
 **July:**
-- Birthdays: Aakash Khaunte (6th), Dhruv Rajani (10th).
+- Birthdays: Aakash Khaunte (6th), Dhruv Rajani (10th), Aparna (Aakash Khaunte's spouse, 14th), Shibani (Gaurang Suctancar's spouse, 26th), Imara (Ali Haji's child, 29th), Kian (Nitin Bhatia's child, 15th), Naimah (Neha Bhasin's child, 3rd).
+- Anniversaries: Arpit & Arushi Agarwal (6th).
 
 **August:**
-- Birthdays: Siddharth Goel (12th), Rahul Agarwal (13th), Sunaina Gera (16th), Karen Bhatia (28th), Priya Agarwal (Spouse, 25th).
+- Birthdays: Siddharth Goel (12th), Rahul Agarwal (13th), Sunaina Gera (16th), Karen Bhatia (28th), Priya (Rahul Agarwal's spouse, 25th), Shweta (Vikram Puri's spouse, 27th), Ansh (Aakash Khaunte's child, 10th), Rishabh (Gaurang Suctancar's child, 28th), Rehaan (Asheen Laljee's child, 25th), Kiara (Nitin Bhatia's child, 25th), Mohit (Haresh Abichandani's child, 27th), Aarna (Amit Palekar's child, 5th).
+- Anniversaries: Venkat & Sneha Muppana (9th), Priya & Gaurav Bakshi (1st), Mangish & Mayura Kakodkar (1st).
 
 **September:**
-- Birthdays: Arushi Agarwal (Spouse, 30th), Rasika Palekar (Spouse, 27th).
+- Birthdays: Rasika (Amit Palekar's spouse, 27th), Arushi (Arpit Agarwal's spouse, 30th), Gaurav (Priya Coulagi Bakshi's spouse, 30th), Arohee (Attreya Sawantt's child, 7th), Shiv (Siddharth Goel's child, 3rd), Priyanshi (Gaurav Sapra's child, 3rd), Reanna (Rahul Khetrapal's child, 17th), Lyla (Kavita Coulagi's child, 12th).
 - Anniversaries: Neha Bhasin & Harshvardhan Sharma (20th).
 
 **October:**
-- Birthdays: Vartan Mathias (1st), Ashwin Khalap (25th), Richa Agrawal (Spouse, 28th), Sneha Muppana (Spouse, 4th).
+- Birthdays: Vartan Mathias (1st), Gaurav Sapra (1st), Ashwin Khalap (25th), Rijul Bharat (23rd), Richa (Anirudh Agrawal's spouse, 28th), Sneha (Venkat Muppana's spouse, 4th), Padmaraje (Gopal Patwardhan's spouse, 27th), Vion (Vartan Mathias's child, 9th).
 
 **November:**
-- Birthdays: Ali Haji (24th), Anuj Kurade (20th), Neha Bhasin (14th), Harshvardhan Sharma (Spouse, 26th).
+- Birthdays: Ali Haji (24th), Anuj Kurade (20th), Neha Bhasin (14th), Harshvardhan (Neha Bhasin's spouse, 26th), Mahira (Rahul Khetrapal's child, 18th), Aneesh (Sandeep Verenkar's child, 28th).
 - Anniversaries: Aakash & Aparna Khaunte (20th), Ali & Richa Haji (11th), Anirudh & Richa Agrawal (25th).
 
 **December:**
-- Birthdays: Arpit Agarwal (8th), Sandeep Verenkar (6th), Radha Goel (Spouse, 8th), Divya Sapra (Spouse, 22nd).
-- Anniversaries: Atul & Gaurapriya Pai Kane (27th), Nitin & Karen Bhatia (21st).
+- Birthdays: Arpit Agarwal (8th), Sandeep Verenkar (6th), Vikram Puri (12th), Radha (Siddharth Goel's spouse, 8th), Divya (Gaurav Sapra's spouse, 22nd), Shruti (Sandeep Agarwal's spouse, 10th), Nandini (Neha Bhasin's child, 24th), Khushi (Haresh Abichandani's child, 13th), Rohan (Rahul Agarwal's child, 15th), Mansi (Tony Dewan's child, 9th).
+- Anniversaries: Atul & Gaurapriya Pai Kane (27th), Sandeep & Sonali Verenkar (23rd), Nitin & Karen Bhatia (21st).
 `;
 
 const photoLinks = `
@@ -80,7 +83,8 @@ You are Maria, an AI guide for the Entrepreneurs' Organization (EO) in Goa. Your
 3.  If the answer is not in the context, say you don't have that specific detail right now but enthusiastically pivot to something you DO know (e.g., "I don't have the exact time for that yet, but speaking of exciting events, did you know we're going to Malta?!").
 4.  Keep your answers conversational and concise. Format lists with bullet points for readability.
 5.  When asked for events, birthdays, or anniversaries in a specific month, list all relevant items from that month from the context.
-6.  Do not mention SPOCs, champions, or event status (like 'On Track' or 'Complete') unless it's a key part of the description. Keep it natural.
+6.  When mentioning a spouse or child, use the relational format provided (e.g., "Samvedita, Varun Chawla's spouse").
+7.  Do not mention SPOCs, champions, or event status (like 'On Track' or 'Complete') unless it's a key part of the description. Keep it natural.
 
 **Context - EO Goa Information:**
 ---
@@ -93,7 +97,7 @@ It's an exclusive process involving interviews. You must be a founder, co-founde
 **Events Calendar:**
 ${eventsInfo}
 
-**Member & Spouse Celebrations (Birthdays & Anniversaries):**
+**Member, Spouse & Children Celebrations (Birthdays & Anniversaries):**
 ${birthdaysAndAnniversariesInfo}
 
 **Photo & Video Links:**
